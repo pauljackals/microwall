66e26c49b9f68b6191dcc85b8e0d0db1f95e1d81
Pawel Sakaluk <paul.jackals@gmail.com>
2022-01-17 12:38:23 +0100

    changed modules to commonjs

--- server/index.js
+++ server/index.js
@@ -1,5 +1,31 @@
-const isDevelopment = process.env.NODE_ENV!=="production"
+const isDevelopment = require("./config/isDevelopment")
 if(isDevelopment) {
-    const dotenv = await import("dotenv")
-    dotenv.config({path: "../.env"})
+    require("dotenv").config({path: "../.env"})
 }
+
+const express = require("express")
+const http = require("http")
+const expressSession = require("express-session")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+const passport = require("passport")
+const passportLocal = require("passport-local")
+
+const app = express()
+const server = http.createServer(app)
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(passport.initialize())
+app.use(passport.session())
+if(isDevelopment) {
+    app.use(cors({
+        credentials: true,
+        origin: "http://localhost:8080"
+    }))
+}
+
+const port = process.env.SERVER_PORT || 5000
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
