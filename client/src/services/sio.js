d82abfb94093fcc7ff6829890f8c08cbdcff21a1
Pawel Sakaluk <paul.jackals@gmail.com>
2022-02-10 13:02:06 +0100

    made vue ports configurable

--- client/src/services/sio.js
+++ client/src/services/sio.js
@@ -1,7 +1,7 @@
 import {io} from "socket.io-client"
 import isDevelopment from "../utils/isDevelopment"
 
-const baseURL = isDevelopment ? `http://localhost:5000` : ''
+const baseURL = isDevelopment ? `http://localhost:${process.env.VUE_APP_SERVER_PORT}` : ''
 const baseOptions = {
     withCredentials: true
 }
---
+++ client/vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    devServer: {
+        port: process.env.VUE_APP_CLIENT_PORT || 8080
+    }
+}
